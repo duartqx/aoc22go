84173dff9625214f029c0b24014016e489e9ab15
Diego Duarte <duartqx@gmail.com>
2023-08-14 22:50:05 -0300

    day8 task1 example correct

---
+++ day8/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+func toInt(b byte) int {
+	i, _ := strconv.Atoi(string(b))
+	return i
+}
+
+func main() {
+
+	t := `30373
+25512
+65332
+33549
+35390`
+
+	patch_of_trees := strings.Split(t, "\n")
+
+	len_patch_of_trees := (len(patch_of_trees) - 2)
+
+	len_row_of_trees := len(patch_of_trees[0])
+
+	number_of_visible_trees := (len_row_of_trees * 2) + (len_patch_of_trees * 2)
+
+	for i := 1; i < len(patch_of_trees)-1; i++ {
+		for j := 1; j < len(patch_of_trees[0])-1; j++ {
+
+			seen_by_all_sides := true
+			current_tree_height := toInt(patch_of_trees[i][j])
+
+			tblr := []int{
+				toInt(patch_of_trees[i-1][j]),
+				toInt(patch_of_trees[i+1][j]),
+				toInt(patch_of_trees[i][j-1]),
+				toInt(patch_of_trees[i][j+1]),
+			}
+
+			for other_tree := range tblr {
+				if current_tree_height <= other_tree {
+					seen_by_all_sides = false
+				}
+
+			}
+			if seen_by_all_sides {
+				number_of_visible_trees += 1
+			}
+		}
+	}
+	log.Println(number_of_visible_trees)
+}
