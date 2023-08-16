c28467d46f0251d2c2378b77d94c20010e359918
Diego Duarte <duartqx@gmail.com>
2023-08-16 01:52:42 -0300

    day8 task2 more refactor

--- day8/src/task2.go
+++ day8/src/task2.go
@@ -1,91 +1,68 @@
 package day8
 
-import (
-	"slices"
-)
-
-type TreeContext struct {
+type ScenicScore struct {
 	row     int
 	column  int
 	current int
 	grid    *[]string
 }
 
-func (ctx *TreeContext) SetCoords(row, column int) *TreeContext {
-	ctx.row = row
-	ctx.column = column
-	return ctx
+func (s *ScenicScore) SetCoords(row, column int) *ScenicScore {
+	s.row = row
+	s.column = column
+	return s
 }
 
-func (ctx *TreeContext) SetColumn(column int) *TreeContext {
-	ctx.column = column
-	return ctx
+func (s *ScenicScore) Build() *ScenicScore {
+	s.current = byteToInt((*s.grid)[s.row][s.column])
+	return s
 }
 
-func (ctx *TreeContext) SetRow(row int) *TreeContext {
-	ctx.row = row
-	return ctx
-}
-
-func (ctx *TreeContext) Build() *TreeContext {
-	ctx.current = byteToInt((*ctx.grid)[ctx.row][ctx.column])
-	return ctx
-}
-
-func (ctx *TreeContext) getScenicScoreTop() (score int) {
-	for t := ctx.row - 1; t >= 0; t-- {
+func (s *ScenicScore) scoreTop() (score int) {
+	for t := s.row - 1; t >= 0; t-- {
 		score++
-		if byteToInt((*ctx.grid)[t][ctx.column]) >= ctx.current {
+		if byteToInt((*s.grid)[t][s.column]) >= s.current {
 			return score
 		}
 	}
 	return score
 }
 
-func (ctx *TreeContext) getScenicScoreBottom() (score int) {
-	for b := ctx.row + 1; b < len(*ctx.grid); b++ {
+func (s *ScenicScore) scoreBottom() (score int) {
+	for b := s.row + 1; b < len(*s.grid); b++ {
 		score++
-		if byteToInt((*ctx.grid)[b][ctx.column]) >= ctx.current {
+		if byteToInt((*s.grid)[b][s.column]) >= s.current {
 			return score
 		}
 	}
 	return score
 }
 
-func (ctx *TreeContext) getScenicScoreLeft() (score int) {
-	for l := ctx.column - 1; l >= 0; l-- {
+func (s *ScenicScore) scoreLeft() (score int) {
+	for l := s.column - 1; l >= 0; l-- {
 		score++
-		if byteToInt((*ctx.grid)[ctx.row][l]) >= ctx.current {
+		if byteToInt((*s.grid)[s.row][l]) >= s.current {
 			return score
 		}
 	}
 	return score
 }
 
-func (ctx *TreeContext) getScenicScoreRight() (score int) {
-	for r := ctx.column + 1; r < len((*ctx.grid)[0]); r++ {
+func (s *ScenicScore) scoreRight() (score int) {
+	for r := s.column + 1; r < len((*s.grid)[0]); r++ {
 		score++
-		if byteToInt((*ctx.grid)[ctx.row][r]) >= ctx.current {
+		if byteToInt((*s.grid)[s.row][r]) >= s.current {
 			return score
 		}
 	}
 	return score
 }
 
-type ScenicScore struct {
-	top    int
-	bottom int
-	left   int
-	right  int
-}
-
-func (s *ScenicScore) getScore() int {
-	return s.top * s.bottom * s.left * s.right
+func (s *ScenicScore) GetScore() int {
+	return s.scoreTop() * s.scoreBottom() * s.scoreLeft() * s.scoreRight()
 }
 
 func Task2(patch_of_trees *[]string) (score int) {
-
-	scenic_scores := []int{}
 
 	c := make(chan int)
 
@@ -93,21 +70,14 @@ func Task2(patch_of_trees *[]string) (score int) {
 
 		defer close(c)
 
-		ctx := TreeContext{grid: patch_of_trees}
+		s := ScenicScore{grid: patch_of_trees}
 
 		for i := 0; i < len(*patch_of_trees); i++ {
 			for j := 0; j < len((*patch_of_trees)[0]); j++ {
 
-				ctx.SetCoords(i, j).Build()
+				s.SetCoords(i, j).Build()
 
-				ss := ScenicScore{
-					top:    ctx.getScenicScoreTop(),
-					bottom: ctx.getScenicScoreBottom(),
-					left:   ctx.getScenicScoreLeft(),
-					right:  ctx.getScenicScoreRight(),
-				}
-
-				c <- ss.getScore()
+				c <- s.GetScore()
 			}
 		}
 
@@ -115,8 +85,10 @@ func Task2(patch_of_trees *[]string) (score int) {
 	}()
 
 	for s := range c {
-		scenic_scores = append(scenic_scores, s)
+		if score < s {
+			score = s
+		}
 	}
 
-	return slices.Max(scenic_scores)
+	return score
 }
